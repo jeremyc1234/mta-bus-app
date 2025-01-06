02ec8b45d61fc717dcc8e38c826b8f7d7fdc42f4
Jeremy <1999jeremycho@gmail.com>
2025-01-06 09:47:02 -0500

    metadata update

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,4 +1,4 @@
 export const metadata = {
-    title: 'MTA Bus Finder',
+    title: 'myMTAbus - NYC MTA Bus Finder',
     description: 'Find New York City MTA bus route location and arrival information.',
   };
