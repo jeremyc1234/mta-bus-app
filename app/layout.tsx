c86e9c56e1c594400a407bb2de959fd75c7fa8e5
Jeremy <1999jeremycho@gmail.com>
2025-01-06 02:07:54 -0500

    gtm added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,20 +7,31 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "MTA Bus Finder",
   description:
-    "Find New York City MTA bus route location and arrival information. Either use your location, select from a list of NYC tourist attractions and locations, or type in an address to find the nearest bus routes and information.",
+    "Find New York City MTA bus route location and arrival information.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" style={{ height: "100%" }}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MLQ6ZJX7');
+            `,
+          }}
+        />
         <style>{`
           body {
             margin: 0;
             padding: 0;
             overflow-x: hidden;
           }
-          
           * {
             box-sizing: border-box;
           }
@@ -38,6 +49,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           width: "100%"
         }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MLQ6ZJX7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         <main style={{ 
           flex: 1, 
