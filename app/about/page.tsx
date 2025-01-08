90db7ad360d51eb54757afe78b310eaecd5fc522
Jeremy <1999jeremycho@gmail.com>
2025-01-07 23:05:36 -0500

    updated about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,7 +38,7 @@ export default function About() {
           textAlign: "center",
         }}
       >
-        Welcome to MTA Bus Finder! This application helps you track New York City buses
+        Welcome to MTA Bus Finder! This app helps you track New York City buses
         in real-time, providing up-to-date information about bus arrivals and departures
         across the city&apos;s extensive network.
       </p>
