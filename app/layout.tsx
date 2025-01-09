b3fe90785789a7ad28023351d1c181a2159215ba
Jeremy <1999jeremycho@gmail.com>
2025-01-09 12:07:51 -0500

    gtm javascript change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-// app/layout.tsx
 import { Inter } from 'next/font/google';
 import Header from './header';
 import Link from 'next/link';
 import { Providers } from './providers';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 const GTM_ID = 'GTM-MLQ6ZJX7';
@@ -37,7 +37,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             }
           `}
         </style>
-        <script 
+        <Script 
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.addEventListener('DOMContentLoaded', (event) => {
+                window.onload = function() {
+                  setTimeout(function() {
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=XXXXXXXXXXXXX&gtm_preview=env-2&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${GTM_ID}');
+                  }, 5000);
+                };
+              });
+            `
+          }}
+        />
+        <Script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9527056951697290"
           crossOrigin="anonymous"
